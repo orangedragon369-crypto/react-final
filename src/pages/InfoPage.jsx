d30738c6843f4351918cfa58d62c6adde674a6c0
Benjamin Ludington <orangedragon369@gmail.com>
2025-11-20 10:20:58 -0700

    Update InfoPage component structure and content

--- src/pages/InfoPage.jsx
+++ src/pages/InfoPage.jsx
@@ -1,5 +1,20 @@
+import "../style/index.css";
+
 export default function infoPage(){
     return (
-        <main className="info-main"></main>
+        <main className="info-main">
+            <div className="filed">
+                <div><span>Monster Hunter&ensp;</span><span>&gt;</span></div>
+                <div>
+                    <div><span>Monster Hunter World&ensp;</span><span>&gt;</span></div>
+                    <div><span>Monster Hunter Wilds&ensp;</span><span>&gt;</span></div>
+                </div>
+                <div><span>Dungeons & Dragons&ensp;</span><span>&gt;</span></div>
+                <div>
+
+                </div>
+            </div>
+            <div className="data"></div>
+        </main>
     );
 }
