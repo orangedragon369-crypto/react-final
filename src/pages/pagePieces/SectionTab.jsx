c469723ce044587eb3a7dc84b093eb4a1b7ccea8
Benjamin Ludington <orangedragon369@gmail.com>
2025-11-21 10:39:32 -0700

    adding info navigation ui

---
+++ src/pages/pagePieces/SectionTab.jsx
@@ -0,0 +1,13 @@
+import "../../style/index.css";
+import infoButton from "../../scriptsOnly/infoButton.js";
+
+export default function SectionTab({name, id, parrentId, ulContents}){
+    return (
+        <>
+        <div><button id={`${parrentId}${id}`} value="closed" className="infoBtn" onClick={() => infoButton(parrentId + id)}><span id={`${parrentId}${id}stat`}>&gt;</span><span>&ensp; {name} </span></button></div>
+        <ul id={parrentId + id + "C"} className="closed">
+            {ulContents}
+        </ul>
+        </>
+    );
+}
